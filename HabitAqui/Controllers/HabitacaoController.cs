0a26f980ac2339e498fbef5eed21f3cce18d8fda
Filipe Carvalho <ftcarvalho@gmail.com>
2023-10-14 19:14:11 +0100

    logos, cores e página Habitações

---
+++ HabitAqui/Controllers/HabitacaoController.cs
@@ -0,0 +1,19 @@
+﻿using HabitAqui.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HabitAqui.Controllers;
+
+public class HabitacaoController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public HabitacaoController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
