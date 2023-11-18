4ca74dd3ece56847fc78763e4cc979a792749944
Filipe Carvalho <ftcarvalho@gmail.com>
2023-11-18 00:57:13 +0000

    Área de administração com dummy content.

--- HabitAqui/Models/DetalhesUtilizador.cs
+++ HabitAqui/Models/DetalhesUtilizador.cs
@@ -5,14 +5,10 @@ namespace HabitAqui.Models;
 
 public class DetalhesUtilizador : IdentityUser
 {
-    //[Key] public int Id { get; set; }
-
     [Required] public string Nome { get; set; }
 
     [Required] public string Apelido { get; set; }
 
-    //[Required] public string Email { get; set; }
-    //[Required] public string Telefone { get; set; }
     [Required] public string Nif { get; set; }
 
     public int LocalizacaoId { get; set; }
