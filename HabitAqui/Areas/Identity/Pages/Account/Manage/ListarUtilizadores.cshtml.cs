cd37fb0757f083dc56bf2b82ad63af8cdb391dbb
mcbride96 <mcbride9696@gmail.com>
2023-11-20 21:21:03 +0000

    feito

---
+++ HabitAqui/Areas/Identity/Pages/Account/Manage/ListarUtilizadores.cshtml.cs
@@ -0,0 +1,37 @@
+using HabitAqui.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HabitAqui.Areas.Identity.Pages.Account.Manage
+{
+    public class ListarUtilizadoresModel : PageModel
+    {
+        //injeção de dependencias para aceder ao IdentityUser
+        private readonly UserManager<DetalhesUtilizador> UserManager;
+        public ListarUtilizadoresModel(UserManager<DetalhesUtilizador> userManager)
+        {
+            this.UserManager = userManager;
+        }
+
+        public IList<UtilizadorComFuncao> Users { get; set; }
+        public class UtilizadorComFuncao
+        {
+            public string Id { get; set; }
+            public string UserName { get; set; }
+            public string Email { get; set; }
+            public IList<string> Roles { get; set; }
+            // Adicione mais propriedades conforme necessário
+        }
+        public void OnGet()
+        {
+            Users = UserManager.Users.Select(u => new UtilizadorComFuncao
+            {
+                Id = u.Id,
+                UserName = u.UserName,
+                Email = u.Email,
+                Roles = UserManager.GetRolesAsync(u).Result.ToList()
+            }).ToList();
+        }
+    }
+}
