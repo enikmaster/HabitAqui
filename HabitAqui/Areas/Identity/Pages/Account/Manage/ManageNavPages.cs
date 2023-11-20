fd2f28a39975f405c95c4774ccf31fb6746cbb67
KarvelMarques <97770700+KarvelMarques@users.noreply.github.com>
2023-11-20 10:05:10 +0000

    Listagem de todos os utilizadores. Ordenação por nome e por role. Falta para de edição

--- HabitAqui/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ HabitAqui/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -35,6 +35,14 @@ public static class ManageNavPages
     ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
     ///     directly from your code. This API may change or be removed in future releases.
     /// </summary>
+    /// 
+    public static string ListarUtilizadores => "ListarUtilizadores";
+
+    /// <summary>
+    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+
     public static string DownloadPersonalData => "DownloadPersonalData";
 
     /// <summary>
@@ -68,6 +76,15 @@ public static class ManageNavPages
     public static string IndexNavClass(ViewContext viewContext)
     {
         return PageNavClass(viewContext, Index);
+    }
+
+    /// <summary>
+    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    public static string ListarUtilizadorNavClass(ViewContext viewContext)
+    {
+        return PageNavClass(viewContext, ListarUtilizadores);
     }
 
     /// <summary>
