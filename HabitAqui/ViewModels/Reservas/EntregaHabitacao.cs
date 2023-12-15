d95ae57db2cfecb87b8bfe1fc0f6e1ae0431539c
KarvelMarques <97770700+KarvelMarques@users.noreply.github.com>
2023-12-15 14:10:59 +0000

    feat: Continuação arrendamento.

---
+++ HabitAqui/ViewModels/Reservas/EntregaHabitacao.cs
@@ -0,0 +1,21 @@
+﻿using HabitAqui.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HabitAqui.ViewModels.Reservas
+{
+    public class EntregaHabitacao
+    {
+        public string FuncionarioId { get; set; }
+        public DateTime DataEntrega {  get; set; }
+
+        public TipoTransacao TipoTransacao { get; set; }
+
+        public int ReservaId { get; set; }
+        public bool Danos { get; set; }
+        public string Observacoes { get; set; }
+        public ICollection<Opcional>? Opcionais { get; set; }
+        public ICollection<ImagemRegistoEntrega>? Imagens { get; set; }
+
+
+    }
+}
