104317a0036637cc45e450a20584c2ebbd46fcc6
mcbride9696 <mcbride9696@gmail.com>
2023-12-16 13:24:56 +0000

    H-32 Sorting e Paging na area de Administracao

--- HabitAqui/Areas/Identity/Pages/Account/Manage/GestaoCategorias.cshtml.cs
+++ HabitAqui/Areas/Identity/Pages/Account/Manage/GestaoCategorias.cshtml.cs
@@ -1,42 +1,83 @@
 using HabitAqui.Data;
 using HabitAqui.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace HabitAqui.Areas.Identity.Pages.Account.Manage;
-
-public class GestaoCategoriasModel : PageModel
+namespace HabitAqui.Areas.Identity.Pages.Account.Manage
 {
-    private readonly ApplicationDbContext _context;
-
-    public GestaoCategoriasModel(ApplicationDbContext context)
+    public class GestaoCategoriasModel : PageModel
     {
-        _context = context;
-    }
+        private readonly ApplicationDbContext _context;
 
-    public List<Categoria> Categorias { get; set; }
-
-    public Dictionary<int, int> TotalHabitacoesPorCategoria { get; set; }
-
-    public void OnGet()
-    {
-        Categorias = _context.Categorias.ToList();
-
-        // Inicializar o dicionário
-        TotalHabitacoesPorCategoria = new Dictionary<int, int>();
-
-        // Para cada categoria, obter o total de habitações e armazenar
-        foreach (var categoria in Categorias)
+        public GestaoCategoriasModel(ApplicationDbContext context)
         {
-            var totalHabitacoes = _context.Habitacoes
-                .Count(h => h.Categorias.Any(c => c.Id == categoria.Id));
-            TotalHabitacoesPorCategoria[categoria.Id] = totalHabitacoes;
+            _context = context;
         }
-    }
 
-    public int GetTotalHabitacoesComCategoria(int categoriaId)
-    {
-        // Verifique se a categoriaId existe no dicionário TotalHabitacoesPorCategoria
-        if (TotalHabitacoesPorCategoria.ContainsKey(categoriaId)) return TotalHabitacoesPorCategoria[categoriaId];
-        return 0;
+        // Properties for categories and their corresponding total habitations
+        public List<Categoria> Categorias { get; set; }
+        public Dictionary<int, int> TotalHabitacoesPorCategoria { get; set; }
+
+        // Sorting properties
+        public string CurrentSort { get; set; }
+        public string IdSort { get; set; }
+        public string NomeSort { get; set; }
+        public string TotalHabitacoesSort { get; set; }
+
+        // Pagination properties
+        public int CurrentPage { get; set; } = 1;
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; } = 10; // Adjust page size as needed
+
+        public void OnGet(string sortOrder, int? pageNumber)
+        {
+            CurrentSort = sortOrder;
+            IdSort = String.IsNullOrEmpty(sortOrder) ? "id_desc" : "";
+            NomeSort = sortOrder == "Nome" ? "nome_desc" : "Nome";
+            TotalHabitacoesSort = sortOrder == "TotalHabitacoes" ? "totalHabitacoes_desc" : "TotalHabitacoes";
+
+            CurrentPage = pageNumber ?? 1;
+            var totalCategoriesCount = _context.Categorias.Count();
+            TotalPages = (int)Math.Ceiling(totalCategoriesCount / (double)PageSize);
+
+            IQueryable<Categoria> categoriaQuery = _context.Categorias.AsQueryable();
+
+            switch (sortOrder)
+            {
+                case "id_desc":
+                    categoriaQuery = categoriaQuery.OrderByDescending(c => c.Id);
+                    break;
+                case "Nome":
+                    categoriaQuery = categoriaQuery.OrderBy(c => c.Nome);
+                    break;
+                case "nome_desc":
+                    categoriaQuery = categoriaQuery.OrderByDescending(c => c.Nome);
+                    break;
+                case "TotalHabitacoes":
+                    categoriaQuery = categoriaQuery.OrderBy(c =>
+                        _context.Habitacoes.Count(h => h.Categorias.Any(cat => cat.Id == c.Id)));
+                    break;
+                case "totalHabitacoes_desc":
+                    categoriaQuery = categoriaQuery.OrderByDescending(c =>
+                        _context.Habitacoes.Count(h => h.Categorias.Any(cat => cat.Id == c.Id)));
+                    break;
+                default:
+                    categoriaQuery = categoriaQuery.OrderBy(c => c.Id);
+                    break;
+            }
+
+            categoriaQuery = categoriaQuery.Skip((CurrentPage - 1) * PageSize).Take(PageSize);
+            Categorias = categoriaQuery.ToList();
+
+            TotalHabitacoesPorCategoria = new Dictionary<int, int>();
+            foreach (var categoria in Categorias)
+            {
+                var totalHabitacoes = _context.Habitacoes
+                    .Count(h => h.Categorias.Any(cat => cat.Id == categoria.Id));
+                TotalHabitacoesPorCategoria[categoria.Id] = totalHabitacoes;
+            }
+        }
     }
 }
--- HabitAqui/Areas/Identity/Pages/Account/Manage/GestaoLocadores.cshtml.cs
+++ HabitAqui/Areas/Identity/Pages/Account/Manage/GestaoLocadores.cshtml.cs
@@ -5,161 +5,214 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Linq;
 
-namespace HabitAqui.Areas.Identity.Pages.Account.Manage;
-
-public enum EstadoSubscricao
+namespace HabitAqui.Areas.Identity.Pages.Account.Manage
 {
-    Ativo,
-    Suspenso,
-    Cancelado
-}
-
-public class GestaoLocadoresModel : PageModel
-{
-    private readonly ApplicationDbContext _context;
-
-    private readonly IUserEmailStore<DetalhesUtilizador> _emailStore;
-
-    private readonly UserManager<DetalhesUtilizador> _userManager;
-    private readonly IUserStore<DetalhesUtilizador> _userStore;
-
-    public GestaoLocadoresModel(
-        ApplicationDbContext context,
-        UserManager<DetalhesUtilizador> userManager,
-        IUserStore<DetalhesUtilizador> userStore)
+    public enum EstadoSubscricao
     {
-        _context = context;
-        _userManager = userManager;
-        _userStore = userStore;
-        _emailStore = GetEmailStore();
+        Ativo,
+        Suspenso,
+        Cancelado
     }
 
-    [BindProperty] public InputModel Input { get; set; }
-    [TempData] public string StatusMessage { get; set; }
-    public IList<Locador> Locadores { get; set; }
-    public IList<DetalhesUtilizador> Gestores { get; set; }
-
-    private async Task LoadAsync(int page, int pageSize)
+    public class GestaoLocadoresModel : PageModel
     {
-        Locadores = await _context.Locadores
-            .Where(l => l.Active)
-            .Include(response => response.Administradores)
-            .Include(response => response.Habitacoes)
-            .ToListAsync();
-        Gestores = await _userManager.GetUsersInRoleAsync(Roles.Gestor.ToString());
-        Input = new InputModel();
-    }
+        private readonly ApplicationDbContext _context;
+        private readonly IUserEmailStore<DetalhesUtilizador> _emailStore;
+        private readonly UserManager<DetalhesUtilizador> _userManager;
+        private readonly IUserStore<DetalhesUtilizador> _userStore;
 
-    public async Task<IActionResult> OnGetAsync()
-    {
-        await LoadAsync(1, 10);
-        return Page();
-    }
-
-    public async Task<IActionResult> OnPostAsync()
-    {
-        if (!ModelState.IsValid) return RedirectToPage();
-
-        var locador = await CreateLocador();
-        SetUserProperties(locador);
-        var result = await _userManager.CreateAsync(locador, Input.Password);
-        if (result.Succeeded)
+        public GestaoLocadoresModel(ApplicationDbContext context, UserManager<DetalhesUtilizador> userManager, IUserStore<DetalhesUtilizador> userStore)
         {
-            await _userManager.AddToRoleAsync(locador, Roles.Gestor.ToString());
-            var gestor = await CreateGestor(Roles.Gestor.ToString());
-            SetUserProperties(gestor);
-            var resultGestor = await _userManager.CreateAsync(gestor, Input.Password);
-            if (resultGestor.Succeeded)
+            _context = context;
+            _userManager = userManager;
+            _userStore = userStore;
+            _emailStore = GetEmailStore();
+            Input = new InputModel(); // Initialize Input here
+        }
+
+        // Sorting properties
+        public string EstadoSubscricaoSort { get; set; }
+        public string IdSort { get; set; }
+        public string NomeSort { get; set; }
+        public string EstadoSort { get; set; }
+        public string TotalHabitacoesSort { get; set; }
+        public string AdministradoresSort { get; set; }
+
+        // Pagination properties
+        public int CurrentPage { get; set; } = 1;
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; } = 10;
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public IList<Locador> Locadores { get; set; }
+        public IList<DetalhesUtilizador> Gestores { get; set; }
+
+        private async Task LoadAsync(int page, int pageSize, string sortOrder)
+        {
+            // Initialize sort properties
+            EstadoSubscricaoSort = sortOrder == "EstadoSubscricao" ? "estado_subscricao_desc" : "EstadoSubscricao";
+            IdSort = sortOrder == "Id" ? "id_desc" : "Id";
+            NomeSort = sortOrder == "Nome" ? "nome_desc" : "Nome";
+            EstadoSort = sortOrder == "Estado" ? "estado_desc" : "Estado";
+            TotalHabitacoesSort = sortOrder == "TotalHabitacoes" ? "total_habitacoes_desc" : "TotalHabitacoes";
+            AdministradoresSort = sortOrder == "Administradores" ? "administradores_desc" : "Administradores";
+
+            var locadoresQuery = _context.Locadores.AsQueryable();
+
+            // Apply sorting
+            switch (sortOrder)
             {
-                await _userManager.AddToRoleAsync(gestor, Roles.Gestor.ToString());
-                locador.Administradores.Add(gestor);
-                await _context.SaveChangesAsync();
-                StatusMessage = "Locador e gestor criados com sucesso.";
-                return RedirectToPage();
+                case "estado_subscricao_desc":
+                    locadoresQuery = locadoresQuery.OrderByDescending(l => l.EstadoDaSubscricao);
+                    break;
+                case "Id":
+                    locadoresQuery = locadoresQuery.OrderBy(l => l.Id);
+                    break;
+                case "id_desc":
+                    locadoresQuery = locadoresQuery.OrderByDescending(l => l.Id);
+                    break;
+                // Add more cases for other sorting criteria
+                default:
+                    locadoresQuery = locadoresQuery.OrderBy(l => l.Nome);
+                    break;
             }
 
-            foreach (var error in resultGestor.Errors) ModelState.AddModelError(string.Empty, error.Description);
+            TotalPages = (int)Math.Ceiling(await locadoresQuery.CountAsync() / (double)pageSize);
+            CurrentPage = page;
+
+            Locadores = await locadoresQuery
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .Include(l => l.Administradores)
+                .Include(l => l.Habitacoes)
+                .ToListAsync();
+        }
+
+
+        public async Task<IActionResult> OnGetAsync(int currentPage = 1, string sortOrder = "")
+        {
+            await LoadAsync(currentPage, PageSize, sortOrder);
             return Page();
         }
 
-        foreach (var error in result.Errors) ModelState.AddModelError(string.Empty, error.Description);
-        return Page();
-    }
-
-    private void SetUserProperties(DetalhesUtilizador user)
-    {
-        user.Nome = Input.Nome;
-        user.Apelido = Input.Apelido;
-        user.Email = Input.Email;
-        user.Nif = Input.Nif;
-        user.PhoneNumber = Input.PhoneNumber;
-        user.Localizacao = Input.Localizacao;
-    }
-
-    private async Task<DetalhesUtilizador> CreateGestor(string role)
-    {
-        try
+        public async Task<IActionResult> OnPostAsync(int currentPage = 1, string sortOrder = "")
         {
-            var user = Activator.CreateInstance<DetalhesUtilizador>();
-            user.Active = true;
-            user.UserName = $"{role.ToLower()}_{Input.Email}";
+            if (!ModelState.IsValid)
+            {
+                await LoadAsync(currentPage, PageSize, sortOrder);
+                return Page();
+            }
+
+            var locador = await CreateLocador();
+            SetUserProperties(locador);
+            var result = await _userManager.CreateAsync(locador, Input.Password);
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(locador, Roles.Gestor.ToString());
+                var gestor = await CreateGestor(Roles.Gestor.ToString());
+                SetUserProperties(gestor);
+                var resultGestor = await _userManager.CreateAsync(gestor, Input.Password);
+                if (resultGestor.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(gestor, Roles.Gestor.ToString());
+                    locador.Administradores.Add(gestor);
+                    await _context.SaveChangesAsync();
+                    StatusMessage = "Locador e gestor criados com sucesso.";
+                    return RedirectToPage();
+                }
+
+                foreach (var error in resultGestor.Errors) ModelState.AddModelError(string.Empty, error.Description);
+                return Page();
+            }
+
+            foreach (var error in result.Errors) ModelState.AddModelError(string.Empty, error.Description);
+            return Page();
+        }
+
+        private void SetUserProperties(DetalhesUtilizador user)
+        {
+            user.Nome = Input.Nome;
+            user.Apelido = Input.Apelido;
             user.Email = Input.Email;
-            await _userStore.SetUserNameAsync(user, user.UserName, CancellationToken.None);
-            await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
-
-            return user;
+            user.Nif = Input.Nif;
+            user.PhoneNumber = Input.PhoneNumber;
+            user.Localizacao = Input.Localizacao;
         }
-        catch
+
+        private async Task<DetalhesUtilizador> CreateGestor(string role)
         {
-            throw new InvalidOperationException(
-                $"Não foi possível criar uma instância de '{nameof(DetalhesUtilizador)}'. " +
-                $"Certifique-se que '{nameof(DetalhesUtilizador)}' não é uma classe abstrata e tem um construtor sem parâmetros.");
-        }
-    }
+            try
+            {
+                var user = Activator.CreateInstance<DetalhesUtilizador>();
+                user.Active = true;
+                user.UserName = $"{role.ToLower()}_{Input.Email}";
+                user.Email = Input.Email;
+                await _userStore.SetUserNameAsync(user, user.UserName, CancellationToken.None);
+                await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
 
-    private async Task<Locador> CreateLocador()
-    {
-        try
+                return user;
+            }
+            catch
+            {
+                throw new InvalidOperationException(
+                    $"Não foi possível criar uma instância de '{nameof(DetalhesUtilizador)}'. " +
+                    $"Certifique-se que '{nameof(DetalhesUtilizador)}' não é uma classe abstrata e tem um construtor sem parâmetros.");
+            }
+        }
+
+        private async Task<Locador> CreateLocador()
         {
-            var user = Activator.CreateInstance<Locador>();
-            user.Active = true;
-            user.UserName = Input.Email;
-            user.Email = Input.Email;
-            user.DataInicioSubscricao = DateTime.Now;
-            user.EstadoDaSubscricao = EstadoSubscricao.Ativo.ToString();
-            user.Administradores = Input.Administradores;
-            user.Habitacoes = Input.Habitacoes;
-            await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
-            await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
-            return user;
+            try
+            {
+                var user = Activator.CreateInstance<Locador>();
+                user.Active = true;
+                user.UserName = Input.Email;
+                user.Email = Input.Email;
+                user.DataInicioSubscricao = DateTime.Now;
+                user.EstadoDaSubscricao = EstadoSubscricao.Ativo.ToString();
+                user.Administradores = Input.Administradores;
+                user.Habitacoes = Input.Habitacoes;
+                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                return user;
+            }
+            catch
+            {
+                throw new InvalidOperationException($"Não foi possível criar uma instância de '{nameof(Locador)}'. " +
+                                                    $"Certifique-se que '{nameof(Locador)}' não é uma classe abstrata e tem um construtor sem parâmetros.");
+            }
         }
-        catch
+
+        private IUserEmailStore<DetalhesUtilizador> GetEmailStore()
         {
-            throw new InvalidOperationException($"Não foi possível criar uma instância de '{nameof(Locador)}'. " +
-                                                $"Certifique-se que '{nameof(Locador)}' não é uma classe abstrata e tem um construtor sem parâmetros.");
+            if (!_userManager.SupportsUserEmail)
+                throw new NotSupportedException("O UI padrão requer um armazenamento de utilizador com suporte por email.");
+            return (IUserEmailStore<DetalhesUtilizador>)_userStore;
         }
-    }
 
-    private IUserEmailStore<DetalhesUtilizador> GetEmailStore()
-    {
-        if (!_userManager.SupportsUserEmail)
-            throw new NotSupportedException("O UI padrão requer um armazenamento de utilizador com suporte por email.");
-        return (IUserEmailStore<DetalhesUtilizador>)_userStore;
-    }
+        public class InputModel
+        {
+            [DisplayName("Nome")] public string Nome { get; set; }
+            [DisplayName("Apelido")] public string Apelido { get; set; }
+            [DisplayName("Email")] public string Email { get; set; }
+            [DisplayName("Password")] public string Password { get; set; }
+            [DisplayName("Telemóvel")] public string PhoneNumber { get; set; }
+            [DisplayName("Nif")] public string Nif { get; set; }
+            public Localizacao Localizacao { get; set; } = new();
+            public ICollection<Habitacao> Habitacoes { get; set; } = new List<Habitacao>();
 
-    public class InputModel
-    {
-        [DisplayName("Nome")] public string Nome { get; set; }
-        [DisplayName("Apelido")] public string Apelido { get; set; }
-        [DisplayName("Email")] public string Email { get; set; }
-        [DisplayName("Password")] public string Password { get; set; }
-        [DisplayName("Telemóvel")] public string PhoneNumber { get; set; }
-        [DisplayName("Nif")] public string Nif { get; set; }
-        public Localizacao Localizacao { get; set; } = new();
-        public ICollection<Habitacao> Habitacoes { get; set; } = new List<Habitacao>();
-
-        [DisplayName("Funcionários")]
-        public ICollection<DetalhesUtilizador> Administradores { get; set; } = new List<DetalhesUtilizador>();
+            [DisplayName("Funcionários")]
+            public ICollection<DetalhesUtilizador> Administradores { get; set; } = new List<DetalhesUtilizador>();
+        }
     }
 }
