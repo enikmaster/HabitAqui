fd274af441552ffa2e8632607aa64f44c9d34b61
Filipe Carvalho <ftcarvalho@gmail.com>
2023-12-17 18:51:30 +0000

    [fix]: ficheiros em falta adicionados. chupa Marques!

---
+++ HabitAqui/Controllers/ImagemController.cs
@@ -0,0 +1,33 @@
+﻿using HabitAqui.Data;
+using HabitAqui.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HabitAqui.Controllers;
+
+public class ImagemController : Controller
+{
+    private readonly ApplicationDbContext _context;
+    private readonly ImagemService _imagemService;
+
+    public ImagemController(ApplicationDbContext context, ImagemService imagemService)
+    {
+        _context = context;
+        _imagemService = imagemService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> RemoverImagem([FromBody] string imagemPath, int habitacaoId)
+    {
+        try
+        {
+            var imagem = _context.Imagens.SingleOrDefault(i => i.Path == imagemPath);
+            if (imagem == null) return NotFound();
+            var novaListaImagens = await _imagemService.RemoverImagem(imagem, habitacaoId);
+            return Json(novaListaImagens);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, $"Erro interno: {e.Message}");
+        }
+    }
+}
---
+++ HabitAqui/Services/ImagemService.cs
@@ -0,0 +1,27 @@
+﻿using HabitAqui.Data;
+using HabitAqui.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HabitAqui.Services;
+
+public class ImagemService
+{
+    private readonly ApplicationDbContext _context;
+    public ImagemService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Imagem>> RemoverImagem(Imagem imagem, int habitacaoId)
+    {
+        var habitacao = await _context.Habitacoes
+            .Include(h => h.Imagens)
+            .FirstOrDefaultAsync(h => h.Id == habitacaoId);
+        if (habitacao == null) return null;
+        habitacao.Imagens.Remove(imagem);
+        _context.Imagens.Remove(imagem);
+        await _context.SaveChangesAsync();
+        return habitacao.Imagens.ToList();
+    }
+}
