e3a4273d369ad34bf287b1d6ef709469ad0061a4
Filipe Carvalho <ftcarvalho@gmail.com>
2023-12-17 13:08:06 +0000

    wip: minor fixes

--- HabitAqui/Data/Seeders/ReservaSeeder.cs
+++ HabitAqui/Data/Seeders/ReservaSeeder.cs
@@ -1,12 +1,7 @@
 ﻿using HabitAqui.Data;
 using HabitAqui.Models;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Identity;
-using System;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-
 
 public static class ReservaSeeder
 {
@@ -17,26 +12,35 @@ public static class ReservaSeeder
             var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
             var userManager = serviceProvider.GetRequiredService<UserManager<DetalhesUtilizador>>();
             var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-           // var locador = serviceProvider.GetRequiredService<Locador>();
+
+            // var locador = serviceProvider.GetRequiredService<Locador>();
 
             if (!context.Reservas.Any())
             {
                 var users = await context.DetalhesUtilizadores.ToListAsync();
 
                 var habitacoes = await context.Habitacoes
-                                            .Include(habitacoes => habitacoes.DetalhesHabitacao)
-                                            .Include(habitacoes => habitacoes.Locador)
-                                            .ToListAsync();
+                    .Include(habitacoes => habitacoes.DetalhesHabitacao)
+                    .Include(habitacoes => habitacoes.Locador)
+                    .ToListAsync();
 
                 var locadores = await context.Locadores
-                                             .Include(locadores => locadores.Administradores)
-                                             .ToListAsync();
+                    .Include(locadores => locadores.Administradores)
+                    .ToListAsync();
 
-                var mockReservas =  ReservaMock.GenerateReservasMocks(users, habitacoes, locadores);
+                var mockReservas = ReservaMock.GenerateReservasMocks(users, habitacoes, locadores);
 
-                foreach(var mockReserva in mockReservas)
+                foreach (var mockReserva in mockReservas)
                 {
                     context.Reservas.Add(mockReserva);
+                    await context.SaveChangesAsync();
+                }
+
+                var mockRegistoEntregas = ReservaMock.GenerateRegistoEntregaMocks(mockReservas);
+
+                foreach (var mockRegistoEntrega in mockRegistoEntregas)
+                {
+                    context.RegistoEntregas.Add(mockRegistoEntrega);
                     await context.SaveChangesAsync();
                 }
             }
